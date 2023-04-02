114b439ae0ad48a4baf88846c6d48db8fb64141b
Conner Drake <connerdrake9@gmail.com>
2023-04-01 22:34:09 -0400

    expand menu on hover

---
+++ src/NavBarExpansion.js
@@ -0,0 +1,18 @@
+const NavBarExpansion = ({className, expandableItems, style}) => {
+    const itemsList = [...expandableItems];
+    
+    const divList = itemsList.map((item, index) => (
+        <div key={index}>{item}</div>
+    ));
+
+    const additionalComponentStyles = { backgroundColor: 'white'};
+    const mergedStyles = { ...style, ...additionalComponentStyles };
+
+    return (
+        <div className={className} style={mergedStyles}>
+            {divList}
+        </div>
+    )
+}
+
+export default NavBarExpansion;
