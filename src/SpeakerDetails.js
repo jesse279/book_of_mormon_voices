00e9e7c05a5f5903746e0423c345e26457a85982
jesse279 <64807581+jesse279@users.noreply.github.com>
2023-04-04 16:30:13 -0600

    Moved images inside src folder, added img path to speaker objects

--- src/SpeakerDetails.js
+++ src/SpeakerDetails.js
@@ -15,7 +15,8 @@ const SpeakerDetails = () => {
     });
     
     if (currentSpeaker.length > 0) {
-      setSpeaker(currentSpeaker[0])
+      setSpeaker(currentSpeaker[0]);
+
     }
     else {
       history.push('/Home');
@@ -30,7 +31,9 @@ const SpeakerDetails = () => {
     <div>
       <div>
         <div>
-          Add Image here
+          <img src={require("./images/Pahoran.jpg")}/>
+          
+          { speaker.img }
         </div>
         <div>
           <h3>
