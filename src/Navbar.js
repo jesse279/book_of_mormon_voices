9601d9b5d468f378363325a894f7d72e46cca0d3
Conner Drake <connerdrake9@gmail.com>
2023-04-04 15:26:37 -0400

    fixed nav expandables bug

--- src/Navbar.js
+++ src/Navbar.js
@@ -28,8 +28,8 @@ const Navbar = () => {
    ];
   const speakersExpandableStyle = {
     position: 'absolute',
-    top: speakersTitleRef.current ? speakersTitleRef.current.getBoundingClientRect().bottom + 10 + 'px' : '0',
-    left: speakersTitleRef.current ? speakersTitleRef.current.getBoundingClientRect().left : '0'
+    top: speakersTitleRef.current ? speakersTitleRef.current.getBoundingClientRect().bottom : '0',
+    left: speakersTitleRef.current ? speakersTitleRef.current.getBoundingClientRect().left : '0',
   };
 
   const toolboxTitleRef = useRef(null);
@@ -68,13 +68,17 @@ const Navbar = () => {
         <Link to="/speakers" className='nav-header' ref={speakersTitleRef} onMouseEnter={() => toggleIsHovering(0)} onMouseLeave={() => toggleIsHovering(0)}>
           Speakers
           <span className="down-carrot"> &#9660;</span>
-          <NavBarExpansion className = {isHovering[0] ? 'navbar-expanded-menu' : 'navbar-expanded-menu hidden'} expandableItems={speakersExpandableItems} style={speakersExpandableStyle}></NavBarExpansion>
+          {isHovering[0] && (
+            <NavBarExpansion className = 'navbar-expanded-menu' expandableItems={speakersExpandableItems} style={speakersExpandableStyle}></NavBarExpansion>
+          )}
         </Link>
         <Link to="/faq" className='nav-header'>FAQ</Link>
         <Link to="/toolbox" className='nav-header' ref={toolboxTitleRef} onMouseEnter={() => toggleIsHovering(1)} onMouseLeave={() => toggleIsHovering(1)}>
           Toolbox
           <span className="down-carrot nav-header"> &#9660;</span>
-          <NavBarExpansion className = {isHovering[1] ? 'navbar-expanded-menu' : 'navbar-expanded-menu hidden'} expandableItems={toolboxExpandableItems} style={toolboxExpandableStyle}></NavBarExpansion>
+          {isHovering[1] && (
+              <NavBarExpansion className = 'navbar-expanded-menu' expandableItems={toolboxExpandableItems} style={toolboxExpandableStyle}></NavBarExpansion>        
+          )}
         </Link>
 
       </div>
