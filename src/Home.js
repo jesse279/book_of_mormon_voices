8c6a21a24140c7fa31b9f3137046de97e0532ecc
Conner Drake <connerdrake9@gmail.com>
2023-04-12 18:04:21 -0400

    added links to home banner

--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,37 @@ import SpeakerListShort from "./SpeakerListShort";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+
+  const links = 
+  [  
+    ['Speakers', '/speakers'],
+    ['Timeline', '/toolbox/timeline'],
+    ['Word Cloud', '/toolbox/word-cloud'],
+    ['Connections', '/toolbox/connections'],
+    ['Network Diagram', '/toolbox/network-diagram'],
+    ['Audio', '/toolbox/audio']
+];
+
+  const bannerTextLeftMargin = '5.57281vw';
+  const bannerBulletSymbol = '>'
+
   return ( 
     <div className="home">
       <div className="banner">
-        <h2 className='main-banner-text' style={{ marginRight: '23vw', paddingTop: '2.12862362522vw'}}><em>You've read our words. Pondered our stories.</em></h2>
-        <h1 className='main-banner-text' style={{ marginRight: '35vw', paddingTop: '3.44418537486vw' }}><em>Now, hear our voice</em></h1>
-        <p className='main-banner-text' style={{ marginRight: '37vw', paddingTop: '2.12862362522vw' }}>
+        <h2 className='main-banner-text' style={{ marginLeft: bannerTextLeftMargin, marginRight: '23vw', paddingTop: '3.44418537486vw'}}><em>You've read our words. Pondered our stories.</em></h2>
+        <h1 className='main-banner-text' style={{ marginLeft: bannerTextLeftMargin, marginRight: '35vw', paddingTop: '3.44418537486vw' }}><em>Now, hear our voice</em></h1>
+        <p className='main-banner-text' style={{ marginLeft: bannerTextLeftMargin, marginRight: '43vw', paddingTop: '2.12862362522vw' }}>
           Through careful research, advanced data analyzation, and 
           compelling stylometry, scholars have discovered a fascinating 
           way to help each voice in the Book of Mormon come to life.
         </p>
+        <ol className='main-banner-text' style={{ marginLeft: bannerTextLeftMargin, paddingTop: '3.44418537486vw' }}>
+          {links.map(([text, url], index) => (
+            <Link to={url}>
+              <h2 key={index} style={{ paddingTop: '0.25vw', paddingBottom: '0.25vw'}}>{bannerBulletSymbol} {text}</h2>
+            </Link>
+          ))}
+        </ol>
       </div>
       <div>
         <SpeakerListShort/>
