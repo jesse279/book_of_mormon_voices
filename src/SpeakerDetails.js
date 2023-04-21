0a016528b0363f09f3066226de3de261c29ce2fd
Conner Drake <connerdrake9@gmail.com>
2023-04-20 21:48:14 -0400

    fixed speaker loading issue

--- src/SpeakerDetails.js
+++ src/SpeakerDetails.js
@@ -1,13 +1,34 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate} from "react-router-dom";
+import { useState, useEffect } from 'react';
 import data from "./speaker_data.json";
 import ImageLoader from "./ImageLoader";
+import Collapsible from "./Collapsible";
 import Analysis_Loader from "./Analysis_Loader";
-import Collapsible from './Collapsible.js';
 
 
 const SpeakerDetails = () => {
   const { speaker_tag } = useParams()
   const speakers = data.speakers;
+  const history = useNavigate();
+  const [speaker, setSpeaker] = useState(speakers[0]);
+  
+  useEffect(() => {
+    const currentSpeaker = speakers.filter((speaker) => {
+      return speaker.link === speaker_tag;
+    });
+    
+    if (currentSpeaker.length > 0) {
+      setSpeaker(currentSpeaker[0]);
+
+    }
+    else {
+      history.push('/Home');
+    }
+    
+    // console.log(currentSpeaker);
+  }, []);
+
+
 
   const speakerObject = speakers.reduce((acc, curr) => curr.link === speaker_tag ? curr : acc, null);
 
