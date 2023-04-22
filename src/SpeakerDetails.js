616f9420fd1c6a43a5e6130df441f3e89bc39e5b
Conner Drake <connerdrake9@gmail.com>
2023-04-22 11:12:37 -0400

    added brand colors as vars

--- src/SpeakerDetails.js
+++ src/SpeakerDetails.js
@@ -4,35 +4,17 @@ import data from "./speaker_data.json";
 import ImageLoader from "./ImageLoader";
 import Collapsible from "./Collapsible";
 import Analysis_Loader from "./Analysis_Loader";
-
+import NotFound from "./NotFound";
 
 const SpeakerDetails = () => {
   const { speaker_tag } = useParams()
   const speakers = data.speakers;
   const history = useNavigate();
   const [speaker, setSpeaker] = useState(speakers[0]);
-  
-  useEffect(() => {
-    const currentSpeaker = speakers.filter((speaker) => {
-      return speaker.link === speaker_tag;
-    });
-    
-    if (currentSpeaker.length > 0) {
-      setSpeaker(currentSpeaker[0]);
-
-    }
-    else {
-      history.push('/Home');
-    }
-    
-    // console.log(currentSpeaker);
-  }, []);
-
-
 
   const speakerObject = speakers.reduce((acc, curr) => curr.link === speaker_tag ? curr : acc, null);
 
-  return ( 
+  return ( speakerObject ? 
     <div>
       <div>
         <div className="speakerdetail-imgbox">
@@ -57,7 +39,7 @@ const SpeakerDetails = () => {
       </div>
       
     </div>
-
+  : <NotFound></NotFound>
    );
 }
  
