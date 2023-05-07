20df9573aa01c1dc9766332457fd97cbd702c2f4
Conner Drake <connerdrake9@gmail.com>
2023-05-06 23:36:03 -0400

    formatted speaker details

--- src/SpeakerDetails.js
+++ src/SpeakerDetails.js
@@ -13,30 +13,21 @@ const SpeakerDetails = () => {
   const speakerObject = speakers.reduce((acc, curr) => curr.link === speaker_tag ? curr : acc, null);
 
   return ( speakerObject ? 
-    <div>
-      <div>
-        <div className="speakerdetail-imgbox">
-          
-          <img className="speakerdetail-img" src={ImageLoader(speakerObject.img)}></img>
-          
-        </div>
-        <div className="speaker-content">
-          <h3 className="speaker-title">
-            { speakerObject.name }
-          </h3>
-          <p className="speaker-description">
-            { speakerObject.description }
-          </p>
-        </div>
-      </div>
+    <article className='speaker-details'>
+      <img className="speaker-detail-img" src={ImageLoader(speakerObject.img)}></img>
+        <h3 style={{marginBottom: '1.03423086758vw'}}>
+          { speakerObject.name }
+        </h3>
+        <p className="speaker-description" style={{marginBottom: '1.03423086758vw'}}>
+          { speakerObject.description }
+        </p>
       <div className="speaker-fact">
         { speakerObject.fact_1 && speakerObject.analysis_1 &&  <Collapsible header_text={speakerObject.fact_1} body_text={<Analysis_Loader id={speakerObject.analysis_1}/>}/>}
       </div>
       <div className="speaker-fact">
         { speakerObject.fact_2 && speakerObject.analysis_2 && <Collapsible header_text={speakerObject.fact_2} body_text={<Analysis_Loader id={speakerObject.analysis_2}/>}/>}
       </div>
-      
-    </div>
+    </article>
   : <NotFound></NotFound>
    );
 }
