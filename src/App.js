3a8b7936d421cdce1bc726c15f6409faede90a2d
Conner Drake <connerdrake9@gmail.com>
2023-05-08 23:16:28 -0400

    dynamically render FAQs

--- src/App.js
+++ src/App.js
@@ -34,21 +34,21 @@ const handleNavHoverMenuExpansion = () => {
 
   return (
     <Router>
-        <Navbar className='nav-bar'></Navbar>
-          <Routes>
-            <Route path="/" element={<Home/>}></Route>
-            <Route path="/about" element={<About/>}></Route>
-            <Route path="/faq" element={<FAQ/>}></Route>
-            <Route path="/speakers" element={<SpeakersAll/>}></Route>
-            <Route path="/speakers/:speaker_tag" element={<SpeakerDetails/>}></Route>
-            <Route path="/toolbox" element={<Toolbox/>}></Route>
-            <Route path="/toolbox/timeline" element={<Timeline/>}></Route>
-            <Route path="/toolbox/word-cloud" element={<WordCloud/>}></Route>
-            <Route path="/toolbox/connections" element={<Connections/>}></Route>
-            <Route path="/toolbox/network-diagram" element={<NetworkDiagram/>}></Route>
-            <Route path="/" element={<Home></Home>}></Route>
-            <Route path="*" element={<NotFound/>}></Route>
-          </Routes>
+      <Navbar className='nav-bar'></Navbar>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/faq" element={<FAQ/>}></Route>
+        <Route path="/speakers" element={<SpeakersAll/>}></Route>
+        <Route path="/speakers/:speaker_tag" element={<SpeakerDetails/>}></Route>
+        <Route path="/toolbox" element={<Toolbox/>}></Route>
+        <Route path="/toolbox/timeline" element={<Timeline/>}></Route>
+        <Route path="/toolbox/word-cloud" element={<WordCloud/>}></Route>
+        <Route path="/toolbox/connections" element={<Connections/>}></Route>
+        <Route path="/toolbox/network-diagram" element={<NetworkDiagram/>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="*" element={<NotFound/>}></Route>
+      </Routes>
     </Router>
     
   );
--- src/FAQ.js
+++ src/FAQ.js
@@ -1,34 +1,26 @@
 import Collapsible from './Collapsible.js';
 
 const FAQ = () => {
-  return (  
-    <div className="content first-element-spacing">
-      <h2 className='main-text' style={{marginBottom: '1vw'}}>FAQ</h2>
-      <div className="content">
-        <div className="faq main-text">
-          <Collapsible header_text="What is the story behind Book of Mormon Voices?" body_text="The Book of Mormon contains a collection of stories and testimonies and stories from many different authors in ancient America over the course of hundreds of years"/>
-        </div>
+  function FAQ(header_text, body_text) {
+    this.headerText = header_text;
+    this.bodyText = body_text;
+  }
 
-        <div className="faq main-text">
-          <Collapsible header_text="What technology was used to analyze language data?" body_text="A deep learning language model was trained on the content of each author in the Book of Mormon to map similarities and differences of different authors in the Book of Mormon. See the Network Diagram."/>
-          
+  let FAQs = [
+    new FAQ("What is the story behind Book of Mormon Voices?", "The Book of Mormon contains a collection of stories and testimonies and stories from many different authors in ancient America over the course of hundreds of years"),
+    new FAQ("What technology was used to analyze language data?", "A deep learning language model was trained on the content of each author in the Book of Mormon to map similarities and differences of different authors. See the Network Diagram."),
+    new FAQ("How can I use this data in my own scripture study?", "Each author in the Book of Mormon has a unique viewpoint on our Savior Jesus Christ aquired through their own conversion. We try to gather resources to help people learn and recognize the difference in language between authors to reflect the strengths of their testimony."),
+    new FAQ("Can I request new topics for analysis?", "You may submit ideas through *insert method later*, we will consider all suggestion and appreciate the input to improve Book of Mormon Voices"),
+    new FAQ("How can I access the data used in the analysis?", "We provide access to the data through our *insert api or method of accessing the data*")
+  ];
 
-        </div>
-        
-        <div className="faq main-text">
-          <Collapsible header_text="How can I use this data in my own study?" body_text="Each author in the Book of Mormon has a unique viewpoint on our Savior Jesus Christ aquired through their own conversion. We try to gather resources to help people learn and recognize the difference in language between authors to reflect the strengths of their testimony."/>
-        </div>
-
-        <div className="faq main-text">
-          <Collapsible header_text="Can I request new topics for analysis?" body_text="You may submit ideas through *insert method later*, we will consider all suggestion and appreciate the input to improve Book of Mormon Voices"/>
-        </div>
-
-
-        <div className="faq main-text">
-          <Collapsible header_text="How can I access the data used in the analysis?" body_text="Get Good"/>
-        </div>
-      </div>
-    </div>
+  return ( 
+    <> 
+      <h2 className='main-text first-element-spacing' style={{marginBottom: '1vw'}}>FAQ</h2>
+      {FAQs.map((expandable, index) => (
+        <Collapsible header_text={expandable.headerText} body_text={expandable.bodyText}/>
+      ))}
+    </>
   );
 }
  
