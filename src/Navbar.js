e3b3c778d926f86b6d348a9e2fdd074ec254187a
Conner Drake <connerdrake9@gmail.com>
2023-05-10 20:57:31 -0400

    dynamically render hover items

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,7 +6,13 @@ import NavBarExpansion from './NavBarExpansion';
 import navHamburgerMenu from './images/hamburger-menu.svg';
 
 const Navbar = () => {
-  const [isHovering, setIsHovering] = useState([false, false]);
+  const reactiveHoverItems = ['navbar-speakers-header', 'navbar-toolbox-header', 'navbar-hamburger-header']
+  let reactiveHoverStatesObject = {};
+  const reactiveHoverStates = reactiveHoverItems.forEach((item) => { reactiveHoverStatesObject[item] = false; });
+  const [hoverStates, setHoverStates] = useState(reactiveHoverStatesObject);
+  console.log('IS HOVERING:');
+  console.log(hoverStates);
+  console.log('-----------------------------');
 
   const speakersExpandableItems = [
     'Abinadi', 
@@ -46,11 +52,25 @@ const Navbar = () => {
     '/toolbox/network-diagram'
   ]
   
-  const toggleIsHovering = (index) => {
-    const adjHovering = [...isHovering];
-    adjHovering[index] = !adjHovering[index];
-    let filteredHovering = adjHovering.map((bool, i) => i === index ? bool : false);
-    setIsHovering(filteredHovering);
+  const toggleIsHovering = (event) => {
+    console.log(event.currentTarget.id);
+    const adjHoverStates = {...hoverStates};
+    console.log('ADJ HOVERING (SHOULD BE SAME AS ORIGINAL IS HOVERING)')
+    console.log(adjHoverStates);
+    console.log('---------------------------------');
+    console.log('ELEMENT TO CHANGE: (SHOULD BE EQUAL TO THE ID OF THE HEADER)');
+    console.log(event.currentTarget.id);
+    console.log('-----------------------------------');
+    adjHoverStates[event.currentTarget.id] = !adjHoverStates[event.currentTarget.id];
+    console.log('ADJUSTED HOVERING (SHOULD REFLECT CHANGE IN EXPANDABLE THAT NEEDS TO OCCUR WITH A TRUE VALUE');
+    console.log(adjHoverStates);
+    console.log('-------------------------------------');
+    let filteredHovering = {};
+    Object.entries(adjHoverStates).forEach(([key, value]) => key === event.currentTarget.id ? filteredHovering[key] = value : filteredHovering[key] = false);
+    console.log('FILTERED HOVERING (SHOULD CONTAIN THE OPPOSITE VALUE AS PREV IN THE CURRENT TARGET ID AND ALL FALSE OTHERWISE');
+    console.log(filteredHovering);
+    console.log('-------------------------------------')
+    setHoverStates(filteredHovering);
   }
 
   return (  
@@ -70,19 +90,19 @@ const Navbar = () => {
       <div className="nav-links">
         <Link to="/" className='nav-header'>Home</Link>
         <Link to="/about" className='nav-header'>About</Link>
-        <Link to="/speakers" className='nav-header' onMouseEnter={() => toggleIsHovering(0)} onMouseLeave={() => toggleIsHovering(0)}>
+        <Link to="/speakers" id={reactiveHoverItems[0]} className='nav-header' onMouseEnter={toggleIsHovering} onMouseLeave={toggleIsHovering}>
           Speakers
           <span className="down-carrot"> &#9660;</span>
-          {isHovering[0] && (
-          <NavBarExpansion id='navbar-speakers-expansion' className='navbar-expanded-menu' expandableItems={speakersExpandableItems} expandableLinks={speakersExpandableLinks} style={{width: '28vw', height: '380px', top: '60px', left: '-6.8vw'}}></NavBarExpansion>
+          {hoverStates[reactiveHoverItems[0]] && (
+          <NavBarExpansion className='navbar-expanded-menu' expandableItems={speakersExpandableItems} expandableLinks={speakersExpandableLinks} style={{width: '28vw', height: '380px', top: '60px', left: '-6.8vw'}}></NavBarExpansion>
         )}
         </Link>
         <Link to="/faq" className='nav-header'>FAQ</Link>
-        <Link to="/toolbox" className='nav-header' onMouseEnter={() => toggleIsHovering(1)} onMouseLeave={() => toggleIsHovering(1)}>
+        <Link to="/toolbox" id={reactiveHoverItems[1]} className='nav-header' onMouseEnter={toggleIsHovering} onMouseLeave={toggleIsHovering}>
           Toolbox
           <span className="down-carrot"> &#9660;</span>
-          {isHovering[1] && (
-          <NavBarExpansion id='navbar-toolbox-expansion' className='navbar-expanded-menu' expandableItems={toolboxExpandableItems} expandableLinks={toolboxExpandableLinks} style={{width: '14vw', top: '60px', left: '-7vw'}}></NavBarExpansion>        
+          {hoverStates[reactiveHoverItems[1]] && (
+          <NavBarExpansion className='navbar-expanded-menu' expandableItems={toolboxExpandableItems} expandableLinks={toolboxExpandableLinks} style={{width: '14vw', top: '60px', left: '-7vw'}}></NavBarExpansion>        
         )}
         </Link>
       </div>
